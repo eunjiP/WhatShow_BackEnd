1468a51213013c41436e85b67aa38d56c09acf57
pelshad <akuaru@gmail.com>
2022-08-02 11:08:39 +0900

    API 상세내용 정리중

--- express/Api/api.js
+++ express/Api/api.js
@@ -1,3 +1,5 @@
+//백엔드 서버 키는법, 터미널에서 node api.js 입력
+
 const express = require('express'); //express import
 const app = express(); 
 const uuidAPIKey = require('uuid-apikey'); //uuid 생성 npm import
@@ -6,14 +8,15 @@ const server = app.listen(3001, () => { //백엔드 서버 연결, 성공시 콘
     console.log('Start Server : localhost:3001');
 });
 
-const key = { //uuid 확인
+//console.log(uuidAPIKey.create()); uuid키 생성(테스트라 콘솔로 출력했음, 직접 사용시 DB랑 연결해야됨)
+const key = { //생성된 uuid 정의
     apiKey: 'N624RC7-G5647YE-MKQVWZE-9N1P8A9',
-  uuid: 'a9844c30-814c-43f9-a4ef-be7d4d436429'
+    uuid: 'a9844c30-814c-43f9-a4ef-be7d4d436429'
 }
 
-//console.log(uuidAPIKey.create()); uuid키 생성
 
-app.get('/api/movie/:apikey/:rank', async (req, res) => {
+app.get('/api/movie/:apikey/:rank', async (req, res) => { 
+    //부메랑으로 http://localhost:3001/api/movie/부여받은 앱키/daily or weekly입력시 api 서버 연결
     let {
         apikey,
         rank
@@ -24,7 +27,7 @@ app.get('/api/movie/:apikey/:rank', async (req, res) => {
         res.send('apikey is not valid.');
     } else {
         if(rank == 'daily'){
-            let data = [
+            let data = [//임시로 만든 객체, 차후 DB랑 연동해서 크롤링한 내용 보여줌
                 {title:"한산:용의 출현", release:"2022-07-27", sales:"23,394,725,692"},
                 {title:"미니언즈2", release:"2022-07-20", sales:"14,691,881,707"},
                 {title:"탑건:매버릭", release:"2022-06-22", sales:"75,252,574,786"},
