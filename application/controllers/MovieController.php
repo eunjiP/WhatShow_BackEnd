5804b947de6bf12cfaff8ca872cc8f7c9631c2fd
HYE0139 <h.ye011096@gmail.com>
2022-08-06 16:12:32 +0900

    영화 상영시간

--- application/controllers/MovieController.php
+++ application/controllers/MovieController.php
@@ -51,12 +51,14 @@ class MovieController extends Controller {
 
     //영화 시간 통신하는 api
     public function movieTime() {
-        //영화코드
-        $code = '191634';
-        //지역코드
+        // //영화코드
+        // $code = '191634';
+        // //지역코드
         $regionRootCode = '10';
-        //조회하는 시간
-        $reserveDate = '2022-08-06';
+        // //조회하는 시간
+        // $reserveDate = '2022-08-06';
+        $code = $_GET['code'];
+        $reserveDate = $_GET['date'];
 
         $url = 'https://movie.naver.com/movie/bi/mi/runningJson.naver?code=' . $code . '&regionRootCode=' . $regionRootCode . '&reserveDate=' . $reserveDate;
         $is_post = false;
