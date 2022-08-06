2340e9892c75823dae7fb3f7b18b71f98e550b85
yeongrong-heo <yeongr1203@gmail.com>
2022-08-06 13:32:26 +0900

    add

--- application/controllers/LocationController.php
+++ application/controllers/LocationController.php
@@ -2,14 +2,30 @@
 namespace application\Controllers;
 
 class LocationController extends Controller {
-    public function LocalList() {
-        return $this->model->getLocalList();
+
+    public function LocationList() {
+        $param = [];
+
+        if(isset($_GET["sub_nm"])) {
+            $sub_nm = intval($_GET["sub_nm"]);
+            if($sub_nm > 0) {
+                $param["sub_nm"] = $sub_nm;
+            }
+        } else {
+            if(isset($_GET["region_nm"])) {
+                $param["region_nm"] = $_GET["region_nm"];
+            }
+        }
+        return $this->model->LocationList($param);
     }
 
-    public function LocalList1() {
-        return $this->model->LocalList1();
+    public function option1List() {
+        return $this->model->option1List();
     }
 
+    public function option2List() {
+        return $this->model->option2List();
+    }
 
 
 }
--- application/models/LocationModel.php
+++ application/models/LocationModel.php
@@ -3,36 +3,49 @@ namespace application\models;
 use PDO;
 
 class LocationModel extends Model {
-    public function getLocaList() {
-        $sql =
-        "   SELECT * FROM region_code t1
-            INNER JOIN subregion_code t2
-            ON t1.root_code = t2.root_code
-        ";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt -> execute();
-        return $stmt->fetchAll(PDO::FETCH_OBJ);
-    }
 
     public function LocationList(&$param) {
         $sql = 
         "   SELECT t1.*, t2.sub_nm
-            FROM region_code t1
-            INNER JOIN subregion_code t2
-            ON t1.root_code = t2.root_code
-            WHERE t1.root_code = t2.root_code        
+            FROM region_code t1, subregion_code t2
+            WHERE t1.root_code = t2.root_code   
         ";
+        if(isset($param["sub_nm"])) {
+            $sub_nm = $param["sub_nm"];
+            $sql .= "AND t2.root_code = ${$sub_nm}";
+        } else {
+            if(isset($param["region_nm"])) {
+                $region_nm = $param["region_nm"];
+                $sql .= " AND t1.region_nm = ${$region_nm}";
+            }
+        }
+
         $stmt = $this->pdo->prepare($sql);
         $stmt -> execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
-    // 카테고리 설정
+    // 카테고리 optionList 설정
     public function optionList1() {
-        // $sql =
-        // "   SELECT region_code AS region_nm FROM region_code
-        //     GROUP BY 
-        // ";
+        $sql =
+        "   SELECT region_nm AS optionList1 FROM region_code ";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function optionList2(&$param) {
+        $sql = 
+        "  SELECT t1.sub_nm AS optionList2
+            FROM subregion_code t1
+            INNER JOIN region_code t2 
+            WHERE t1.root_code = :t1.root_code
+            AND t2.root_code = t1.root_code       
+        ";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt-> bindValue(":t1.root_code", $param["t1.root_code"]);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
 
