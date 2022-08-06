c846c2cec3640641a3c77b9881ca2b8c3951e0c6
pelshad <akuaru@gmail.com>
2022-08-06 14:28:02 +0900

    uid

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -3,6 +3,14 @@ namespace application\models;
 use PDO;
 
 class UserModel extends Model{
+    public function sel_user(&$param){
+        $sql = "SELECT uuid FROM t_user
+                WHERE uuid = :uuid";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$param['uuid']]);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
     public function signup(&$param){
         $sql = "INSERT INTO t_user
         (uuid, nickname)
