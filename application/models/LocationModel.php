217cc1c7c84f3a309d192923134787e641b53940
yeongrong-heo <yeongr1203@gmail.com>
2022-08-06 12:32:29 +0900

    수정

--- application/models/LocationModel.php
+++ application/models/LocationModel.php
@@ -3,17 +3,18 @@ namespace application\models;
 use PDO;
 
 class LocationModel extends Model {
-    public function getLocalList() {
+    public function getLocaList() {
         $sql =
-        "   SELECT * FROM region_code
-            INNER JOIN subregion_code
+        "   SELECT * FROM region_code t1
+            INNER JOIN subregion_code t2
+            ON t1.root_code = t2.root_code
         ";
         $stmt = $this->pdo->prepare($sql);
         $stmt -> execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
-    public function LocalList1(&$param) {
+    public function LocationList(&$param) {
         $sql = 
         "   SELECT t1.*, t2.sub_nm
             FROM region_code t1
@@ -24,6 +25,14 @@ class LocationModel extends Model {
         $stmt = $this->pdo->prepare($sql);
         $stmt -> execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    // 카테고리 설정
+    public function optionList1() {
+        // $sql =
+        // "   SELECT region_code AS region_nm FROM region_code
+        //     GROUP BY 
+        // ";
     }
 
 
