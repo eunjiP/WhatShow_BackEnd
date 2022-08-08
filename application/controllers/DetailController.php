5cef3ec58ea33ff7ef444f0e0cf7a53b8c32f37b
HYE0139 <h.ye011096@gmail.com>
2022-08-08 11:59:08 +0900

    영화 정보

--- application/controllers/DetailController.php
+++ application/controllers/DetailController.php
@@ -2,6 +2,16 @@
     namespace application\Controllers;
 
     class DetailController extends Controller {
+        public function movieInfo() {
+            $url = getUrlPaths();
+            if(!isset($url[2])) {
+                exit();
+            }
+
+            $param = ['movie_code' => $url[2]];
+            return $this->model->getMovieInfo($param);
+        }
+
         public function reviewList() {
             $url = getUrlPaths();
             if(!isset($url[2])) {
