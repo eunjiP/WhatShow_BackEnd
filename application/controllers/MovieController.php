daee73490de0aaed1b174402ee9f3083dd2c9e5d
yeongrong-heo <yeongr1203@gmail.com>
2022-08-12 09:48:02 +0900

    am_full

--- application/controllers/MovieController.php
+++ application/controllers/MovieController.php
@@ -25,10 +25,10 @@ class MovieController extends Controller {
     }
 
     //영화진흥원의 박스오피스 TOP10
-    public function boxOffice() {
+    public function boxOffice(&$param) {
         $key = 'de024e41172ba2b7f13cb5d286ad1162';
-        // $targetDt = $param['targetDt'];
-        $targetDt = '20220810';
+        $targetDt = $param['targetDt'];
+        // $targetDt = '20220810';
         $url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=' . $key . '&targetDt=' . $targetDt;
         $is_post = false;
 
@@ -59,11 +59,11 @@ class MovieController extends Controller {
                 fclose($myfile);
 
                 //은지
-                exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
+                // exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
                 //영은
                 // exec('C:\python\python38\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
                 // 영롱
-                // exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShow_BackEnd\application\controllers\movieSummary.py');
+                exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShow_BackEnd\application\controllers\movieSummary.py');
                 // exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
                 // exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
                 
