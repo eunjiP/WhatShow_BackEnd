6c812a406df82c9830cddf839c81fa71882eae95
eunjiP <dae4227@naver.com>
2022-08-16 12:10:24 +0900

    수정

--- application/controllers/MovieController.php
+++ application/controllers/MovieController.php
@@ -36,7 +36,7 @@ class MovieController extends Controller {
     public function boxOffice(&$param) {
         $key = 'de024e41172ba2b7f13cb5d286ad1162';
         $targetDt = $param['targetDt'];
-        // $targetDt = '20220810';
+        // $targetDt = '20220814';
         $url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=' . $key . '&targetDt=' . $targetDt;
         $is_post = false;
 
@@ -67,9 +67,9 @@ class MovieController extends Controller {
                 fclose($myfile);
 
                 //은지
-                //exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
+                exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
                 //영은
-                 exec('C:\python\python38\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
+                //  exec('C:\python\python38\python.exe C:\Apache24\WhatShowBackEnd\application\controllers\movieSummary.py');
                 //영롱
                 // exec('C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe C:\Apache24\WhatShow_BackEnd\application\controllers\movieSummary.py');
                 //경식
